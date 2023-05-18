bbccf364a452a39db5fdc28d3198a862877d5f39
Abrar2209 <132457715+Abrar2209@users.noreply.github.com>
2023-05-18 19:10:47 +0530

    update klaviyodata

--- utils/klaviyoDataSubmission.js
+++ utils/klaviyoDataSubmission.js
@@ -25,20 +25,40 @@ klaviyoQueue.process(async (job) => {
     rows.forEach(async row => {
       const klaviyoData = JSON.parse(row.klaviyoIntegration);
       console.log(klaviyoData.klaviyoListMapping);
-  
+      console.log(klaviyoData.defaultOption);
       const properties = {};
       
-      Object.values(klaviyoData.klaviyoListMapping).slice(1).forEach(mapping => {
+      Object.values(klaviyoData.klaviyoListMapping).forEach(mapping => {
         const formFieldId = mapping.form_field;
         const klaviyoField = mapping.klaviyo_field;
         console.log("formFieldId",formFieldId);
         console.log(klaviyoField);
         console.log(job.data.fields.hasOwnProperty(formFieldId))
+
+         if (mapping.is_fixed && mapping.is_input !== "") {
+        // Field has a fixed value, assign it directly
+        properties[klaviyoField] = mapping.is_input;
+      } else {
+        // Field does not have a fixed value, retrieve from form fields
         if (fieldIds.includes(formFieldId)) {
           const formFieldValue = job.data.fields[formFieldId];
-          properties[klaviyoField] = formFieldValue;
+          if (klaviyoField === "email") {
+            // If the klaviyoField is 'email', store the value in properties and use it as the email
+            properties[klaviyoField] = formFieldValue;
+          } else {
+            // For other fields, store their values normally
+            properties[klaviyoField] = formFieldValue;
+          }
         }
-        
+      }
+      // if (key.startsWith("custom-")) {
+      //   const customFieldKey = key.split("-")[1];
+      //   const customFormFieldId = mapping.form_field;
+      //   if (fieldIds.includes(customFormFieldId)) {
+      //     const customFormFieldValue = job.data.fields[customFormFieldId];
+      //     properties[customFieldKey] = customFormFieldValue;
+      //   }
+      // }
         //if (formFieldId && klaviyoField && job.data.fields.hasOwnProperty(formFieldId)) {
           // const formFieldValue = job.data.fields[formFieldId];
           // //console.log(formFieldValue);
@@ -47,19 +67,19 @@ klaviyoQueue.process(async (job) => {
       });
   
       console.log("IDS: " + formid);
-      console.log(properties);
-  
-      // await Client.createClientSubscription(JSON.stringify({
-      //   data: {
-      //     type: "subscription",
-      //     attributes: {
-      //       list_id: "WFTi2P",
-      //       custom_source: 'Client Subscription',
-      //       email: job.data.fields.Text,
-      //       properties: properties
-      //     }
-      //   }
-      // }), kapikey.klaviyoApipublicKey);
+        console.log(properties);
+    
+      await Client.createClientSubscription(JSON.stringify({
+        data: {
+          type: "subscription",
+          attributes: {
+            list_id: klaviyoData.defaultOption,
+            custom_source: 'Client Subscription',
+            email: properties.email ,
+            properties: properties
+          }
+        }
+      }), kapikey.klaviyoApipublicKey);
     });
   } catch (e) {
     console.log(e);
